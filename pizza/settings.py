56de691627d92a9d45913f83a640043f7e455642
unknown <lwolczynski7@gmail.com>
2020-06-08 16:22:26 -0500

    Settings updated

--- pizza/settings.py
+++ pizza/settings.py
@@ -122,5 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
 
 LOGIN_URL = '/login'
